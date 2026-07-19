e44c9cd2793583ef999f1efad17c35b5660a0079
Lalitupadhyayy <lalitupadhyay522@gmail.com>
2026-07-19 21:46:04 +0530

    Create the model class,learn enity,primary key

---
+++ app/src/main/java/com/example/studentmanagementapp/model/Student.kt
@@ -0,0 +1,15 @@
+package com.example.studentmanagementapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Student(
+
+ @PrimaryKey(autoGenerate = true)
+ val id : Int,
+ val name :  String,
+ val age : Int,
+  val course : String
+
+ )
