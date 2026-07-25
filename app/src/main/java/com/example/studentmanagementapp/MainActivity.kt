cd64cd2a7606e47ef70d6e88be363f57cf8a3802
Lalitupadhyayy <lalitupadhyay522@gmail.com>
2026-07-25 22:12:29 +0530

    connect the mainactivity with viewmodel

--- app/src/main/java/com/example/studentmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/studentmanagementapp/MainActivity.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
 import com.example.studentmanagementapp.ui.theme.StudentManagementAppTheme
+import com.example.studentmanagementapp.viewmodel.StudentViewModel
+import com.example.studentmanagementapp.viewmodel.StudentViewModelFactory
 
 class MainActivity : ComponentActivity() {
+
+    private val studentViewModel: StudentViewModel by viewModels {
+        StudentViewModelFactory(
+            (application as StudentApplication).repository
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             StudentManagementAppTheme {
 
@@ -24,4 +29,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
