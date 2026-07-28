41d3f6ed02d5205e61124a902d4de84c140ec3bd
Lalitupadhyayy <lalitupadhyay522@gmail.com>
2026-07-28 23:20:31 +0530

    Implemented StateFlow for ViewModel UI state management

--- app/src/main/java/com/example/studentmanagementapp/viewmodel/StudentViewModel.kt
+++ app/src/main/java/com/example/studentmanagementapp/viewmodel/StudentViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.studentmanagementapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.studentmanagementapp.model.Student
 import com.example.studentmanagementapp.repository.StudentRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 class StudentViewModel(
     private val repository : StudentRepository
@@ -23,8 +27,12 @@ class StudentViewModel(
         repository.updateStudent(student)
     }
 
-    fun getAllStudents(): Flow<List<Student>> {
-        return repository.getAllStudents()
-    }
+    val students: StateFlow<List<Student>> =
+        repository.getAllStudents()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = emptyList()
+            )
 
 }
